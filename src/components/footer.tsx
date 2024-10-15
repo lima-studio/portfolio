60af4ff409d87eaab1adc57d43c0cc7776e37a3b
pedrohhope <pvpswapedro123@gmail.com>
2024-10-15 12:04:35 -0300

    fix: footer responsive

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -11,11 +11,11 @@ export default function Footer() {
     }
     return (
         <footer className="bg-gray-300 min-h-[600px]  grid grid-cols-1 pb-5 pt-12 px-5 rounded-t-3xl">
-            <div className="md:flex space-y-5 md:space-y-0">
+            <div className="2xl:flex space-y-5 ">
                 <BlurFade inView>
-                    <img src="/logos/Ativo 24.png" alt="Logo" className="h-20"></img>
+                    <img src="/logos/Ativo 24.png" alt="Logo" className="max-h-20"></img>
                 </BlurFade>
-                <BlurFade className="max-w-5xl mx-auto space-y-5" inView>
+                <BlurFade className="2xl:max-w-5xl mx-auto space-y-5" inView>
 
                     <p className="uppercase font-medium text-sm">{t('footer.contact')}</p>
                     <BlurFade inView className="text-4xl md:text-6xl px-0 " >
