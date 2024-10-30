d556942e3bee300f26b1c2da4d833754f4ef213a
RodrLucas <lucasrodriguessantos0@gmail.com>
2024-10-29 22:36:13 -0300

    ajustes de margem

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,165 +6,166 @@ import BlurFade from "./magicui/blur-fade";
 import { FadeText } from "./magicui/fade-text";
 
 export default function Navbar() {
-    const {
-        t,
-        i18n: { language, changeLanguage },
-    } = useTranslation();
-    const [menuOpen, setMenuOpen] = useState(false);
-    const navigate = useNavigate();
-    const location = useLocation();
+  const {
+    t,
+    i18n: { language, changeLanguage },
+  } = useTranslation();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
 
-
-
-    useEffect(() => {
-        if (menuOpen) {
-            document.body.style.overflowY = 'hidden';
-        } else {
-            document.body.style.overflowY = 'auto';
-        }
-        return () => {
-            document.body.style.overflowY = 'auto';
-        };
-    }, [menuOpen]);
-
-
-    const handleNavigation = (hash: string) => {
-        if (location.pathname !== "/") {
-            navigate("/", { replace: true });
-        }
-
-        setMenuOpen(false);
-
-
-        setTimeout(() => {
-            const element = document.querySelector(hash);
-            if (element) {
-                const elementPosition = element.getBoundingClientRect().top + window.scrollY;
-                const offsetPosition = elementPosition - 70;
-
-
-                window.scrollTo({
-                    top: offsetPosition,
-                    behavior: "smooth",
-                });
-            }
-        }, 100);
+  useEffect(() => {
+    if (menuOpen) {
+      document.body.style.overflowY = "hidden";
+    } else {
+      document.body.style.overflowY = "auto";
+    }
+    return () => {
+      document.body.style.overflowY = "auto";
     };
+  }, [menuOpen]);
 
+  const handleNavigation = (hash: string) => {
+    if (location.pathname !== "/") {
+      navigate("/", { replace: true });
+    }
 
-    return (
-        <nav
-            className="h-[230px] py-5 "
-        >
-            <div className="flex justify-between px-5 xl:px-10 2xl:px-14 h-full">
-                <div className="flex flex-col items-end justify-between">
-                    <BlurFade>
-                        <a href="/" className="mixBlendMode">
-                            <img
-                                src="/logos/Ativo 8.png"
-                                alt="Logo"
-                                className="w-5 h-5 md:w-10 md:h-10 object-contain"
-                            />
-                        </a>
-                    </BlurFade>
+    setMenuOpen(false);
 
-                    {/* Menu de navegação para telas grandes */}
-                    <ul className=" font-medium text-end ">
-                        <li>
-                            <a href="/projects" className="relative hover: transition group">
-                                <FadeText
-                                    text={t('navbar.links.works')}
-                                    direction="up"
-                                    framerProps={{
-                                        hidden: { opacity: 0 },
-                                        show: {
-                                            transition: { delay: 0.25 },
-                                        },
-                                    }}
-                                    className="text-xl"
-                                />
-                            </a>
-                        </li>
-                        <li>
-                            <button onClick={() => handleNavigation("#about")} className="relative hover: transition group">
-                                <FadeText
-                                    text={t('navbar.links.about')}
-                                    direction="up"
-                                    framerProps={{
-                                        hidden: { opacity: 0 },
-                                        show: {
-                                            transition: { delay: 0.5 },
-                                        },
-                                    }}
-                                    className="text-xl"
-                                />
-                            </button>
-                        </li>
-                        <li>
-                            <button onClick={() => handleNavigation("#skills")} className="relative hover: transition group">
-                                <FadeText
-                                    text={t('navbar.links.skills')}
-                                    direction="up"
-                                    framerProps={{
-                                        hidden: { opacity: 0 },
-                                        show: {
-                                            transition: { delay: 1 },
-                                        },
-                                    }}
-                                    className="text-xl"
-                                />
-                            </button>
-                        </li>
-                        <li>
-                            <a href="/contact" className="relative hover: transition group">
-                                <FadeText
-                                    text={t('navbar.links.contact')}
-                                    direction="up"
-                                    framerProps={{
-                                        hidden: { opacity: 0 },
-                                        show: {
-                                            transition: { delay: 1.25 },
-                                        },
-                                    }}
-                                    className="text-xl"
-                                />
-                                <span className="absolute left-0 bottom-0 w-full h-[2px] bg-secondary  transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"></span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+    setTimeout(() => {
+      const element = document.querySelector(hash);
+      if (element) {
+        const elementPosition =
+          element.getBoundingClientRect().top + window.scrollY;
+        const offsetPosition = elementPosition - 70;
 
+        window.scrollTo({
+          top: offsetPosition,
+          behavior: "smooth",
+        });
+      }
+    }, 100);
+  };
 
-                <BlurFade className="flex justify-between flex-col">
-                    <div>
-                        <p>bh/mg</p>
-                        <p>{t('navbar.country')}</p>
-                        <div>
-                            <button
-                                className={`${language === 'en-US' ? 'font-bold' : ''}`}
-                                onClick={() => changeLanguage('en-US')}
-                            >
-                                EN
-                            </button>
-                            /
-                            <button
-                                className={`${language === 'pt-BR' ? 'font-bold' : ''}`}
-                                onClick={() => changeLanguage('pt-BR')}
-                            >
-                                PT
-                            </button>
-                        </div>
-                    </div>
-                    <div className="flex space-y-3 flex-col ">
-                        <a href="https://wa.link/df9y7a" target="_blank">
-                            <FaWhatsapp size={30} className="cursor-pointer text-secondary" />
-                        </a>
-                        <a href="http://www.instagram.net/limastdio" target="_blank">
-                            <FaInstagram size={30} className="cursor-pointer text-secondary" />
-                        </a>
-                    </div>
-                </BlurFade>
+  return (
+    <nav className="h-[230px] py-5">
+      <div className="flex justify-between px-[2vw] xl:px-[3vw] 2xl:px-[5vw] h-full">
+        <div className="flex flex-col items-end justify-between">
+          <BlurFade>
+            <a href="/" className="mixBlendMode">
+              <img
+                src="/logos/Ativo 8.png"
+                alt="Logo"
+                className="w-5 h-5 md:w-10 md:h-10 object-contain"
+              />
+            </a>
+          </BlurFade>
+
+          {/* Menu de navegação para telas grandes */}
+          <ul className=" font-medium text-end ">
+            <li>
+              <a href="/projects" className="relative hover: transition group">
+                <FadeText
+                  text={t("navbar.links.works")}
+                  direction="up"
+                  framerProps={{
+                    hidden: { opacity: 0 },
+                    show: {
+                      transition: { delay: 0.25 },
+                    },
+                  }}
+                  className="text-xl"
+                />
+              </a>
+            </li>
+            <li>
+              <button
+                onClick={() => handleNavigation("#about")}
+                className="relative hover: transition group"
+              >
+                <FadeText
+                  text={t("navbar.links.about")}
+                  direction="up"
+                  framerProps={{
+                    hidden: { opacity: 0 },
+                    show: {
+                      transition: { delay: 0.5 },
+                    },
+                  }}
+                  className="text-xl"
+                />
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={() => handleNavigation("#skills")}
+                className="relative hover: transition group"
+              >
+                <FadeText
+                  text={t("navbar.links.skills")}
+                  direction="up"
+                  framerProps={{
+                    hidden: { opacity: 0 },
+                    show: {
+                      transition: { delay: 1 },
+                    },
+                  }}
+                  className="text-xl"
+                />
+              </button>
+            </li>
+            <li>
+              <a href="/contact" className="relative hover: transition group">
+                <FadeText
+                  text={t("navbar.links.contact")}
+                  direction="up"
+                  framerProps={{
+                    hidden: { opacity: 0 },
+                    show: {
+                      transition: { delay: 1.25 },
+                    },
+                  }}
+                  className="text-xl"
+                />
+                <span className="absolute left-0 bottom-0 w-full h-[2px] bg-secondary  transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"></span>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <BlurFade className="flex justify-between flex-col">
+          <div>
+            <p>bh/mg</p>
+            <p>{t("navbar.country")}</p>
+            <div>
+              <button
+                className={`${language === "en-US" ? "font-bold" : ""}`}
+                onClick={() => changeLanguage("en-US")}
+              >
+                EN
+              </button>
+              /
+              <button
+                className={`${language === "pt-BR" ? "font-bold" : ""}`}
+                onClick={() => changeLanguage("pt-BR")}
+              >
+                PT
+              </button>
             </div>
-        </nav >
-    );
+          </div>
+          <div className="flex space-y-3 flex-col ">
+            <a href="https://wa.link/df9y7a" target="_blank">
+              <FaWhatsapp size={30} className="cursor-pointer text-secondary" />
+            </a>
+            <a href="http://www.instagram.net/limastdio" target="_blank">
+              <FaInstagram
+                size={30}
+                className="cursor-pointer text-secondary"
+              />
+            </a>
+          </div>
+        </BlurFade>
+      </div>
+    </nav>
+  );
 }
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -6,61 +6,51 @@ import { Separator } from "@/components/ui/separator";
 import { projects } from "@/projects";
 import { useTranslation } from "react-i18next";
 
-
 export default function ProjectsPage() {
-    const { t } = useTranslation();
-    return (
-        <div>
-            <Navbar />
-            <div className="px-5 mx-auto mb-20 mt-10">
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-5 pb-32 ">
-                    <BlurFade inView>
-                        <h1 className="text-3xl font-semibold" >
-                            {t('projects.title')}
-                        </h1>
-                    </BlurFade>
-                    <BlurFade inView className="space-y-10">
-                        <div>
-                            <p className="text-lg md:text-3xl" >
-                                {t('projects.description')}
-                            </p>
-                        </div>
-
-                        <div>
-                            <a
-                                className="bg-secondary px-6 py-3 text-white rounded-lg mt-5"
-                                href="/contact"
-                            >
-                                {t('projects.button')} +
-                            </a>
-                        </div>
-                    </BlurFade>
-                </div>
-
-                <BlurFade delay={0.2} inView>
-                    <Separator className="bg-gray-300" />
-                </BlurFade>
-
-                <div className="space-y-5 py-16">
-                    <BlurFade inView >
-                        <h2 className="font-mono">{t('projects.visual_identity')}</h2>
-                    </BlurFade>
-
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
-                        {projects.map((project, index) => (
-                            <BlurFade key={index} inView className="h-[450px]">
-                                <ProjectCard
-                                    key={index}
-                                    project={project}
-                                />
-                            </BlurFade>
-                        ))}
-                    </div>
-                </div>
-
+  const { t } = useTranslation();
+  return (
+    <div>
+      <Navbar />
+      <div className="mx-[5%] mb-20 mt-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 pb-32 ">
+          <BlurFade inView>
+            <h1 className="text-3xl font-semibold">{t("projects.title")}</h1>
+          </BlurFade>
+          <BlurFade inView className="space-y-10">
+            <div>
+              <p className="text-lg md:text-3xl">{t("projects.description")}</p>
             </div>
-            <Footer />
 
+            <div>
+              <a
+                className="bg-secondary px-6 py-3 text-white rounded-lg mt-5"
+                href="/contact"
+              >
+                {t("projects.button")} +
+              </a>
+            </div>
+          </BlurFade>
         </div>
-    )
+
+        <BlurFade delay={0.2} inView>
+          <Separator className="bg-gray-300" />
+        </BlurFade>
+
+        <div className="space-y-5 py-16">
+          <BlurFade inView>
+            <h2 className="font-mono">{t("projects.visual_identity")}</h2>
+          </BlurFade>
+
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+            {projects.map((project, index) => (
+              <BlurFade key={index} inView className="h-[450px]">
+                <ProjectCard key={index} project={project} />
+              </BlurFade>
+            ))}
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
