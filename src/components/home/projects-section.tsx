2fc94509c43a0e71904dfea0029993d72f0e4e57
RodrLucas <lucasrodriguessantos0@gmail.com>
2024-12-14 18:22:38 -0300

    Ajustando sessão de projetos

--- src/components/home/projects-section.tsx
+++ src/components/home/projects-section.tsx
@@ -4,44 +4,41 @@ import BlurFade from "../magicui/blur-fade";
 import ProjectCard from "../projects/project-card";
 
 function ProjectsSection({ projects }: { projects: IProjects[] }) {
-    const projectsData = projects.filter(project => project.homepage);
-    const {
-        t
-    } = useTranslation()
+  const projectsData = projects.filter((project) => project.homepage);
+  const { t } = useTranslation();
 
-    return (
-        <section id="projects" className="space-y-5 px-5 mb-20 md:mb-32 ">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-                {projectsData.map((project, index) => (
-                    <BlurFade
-                        key={index}
-                        delay={index * 0.2}
-                        inView
-                        className="h-[400px] md:h-[650px]"
-                    >
-                        <ProjectCard project={project} />
-                    </BlurFade>
-                ))}
-            </div>
+  return (
+    <section id="projects" className="space-y-5 px-5 mb-20 md:mb-32 ">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        {projectsData.map((project, index) => (
+          <BlurFade
+            key={index}
+            delay={index * 0.2}
+            inView
+            className="h-[700px]"
+          >
+            <ProjectCard project={project} />
+          </BlurFade>
+        ))}
+      </div>
 
-            <div className="grid grid-cols-2 gap-5">
-                <BlurFade inView className="flex justify-end">
-                    <span className="text-xl md:text-3xl ">
-                        {t('home.and_stop_here')}
-                    </span>
-                </BlurFade>
-                <a
-                    href="/projects"
-                >
-                    <BlurFade inView className="text-black font-medium text-base bg-gray-300 rounded text-center flex justify-center items-center h-12 ">
-
-                        {t('home.see_more')}
-
-                    </BlurFade>
-                </a>
-            </div>
-        </section >
-    )
+      <div className="grid grid-cols-2 gap-5">
+        <BlurFade inView className="flex justify-end">
+          <span className="text-xl md:text-3xl ">
+            {t("home.and_stop_here")}
+          </span>
+        </BlurFade>
+        <a href="/projects">
+          <BlurFade
+            inView
+            className="text-black font-medium text-base bg-gray-300 rounded text-center flex justify-center items-center h-12 "
+          >
+            {t("home.see_more")}
+          </BlurFade>
+        </a>
+      </div>
+    </section>
+  );
 }
 
 export default ProjectsSection;
--- src/components/projects/project-card.tsx
+++ src/components/projects/project-card.tsx
@@ -7,17 +7,17 @@ export default function ProjectCard({ project }: { project: IProjects }) {
 
   return (
     <a
-      className="rounded-3xl relative overflow-hidden uppercase group cursor-pointer w-full h-full"
+      className="rounded-xl relative overflow-hidden uppercase group cursor-pointer w-full h-full"
       href={`/projects/${project.slug}`}
     >
       <img
-        className="w-full h-full min-w-full min-h-full object-cover rounded-3xl"
+        className="w-full h-full min-w-full min-h-full object-cover rounded-xl"
         src={project.primary_image_url}
         alt={project.name}
       />
 
       {/* Informações que aparecem ao passar o mouse */}
-      <div className="rounded-3xl absolute top-0 left-0 right-0 h-full flex justify-between bg-black bg-opacity-50 p-5 text-white font-bold opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+      <div className="rounded-xl absolute top-0 left-0 right-0 h-full flex justify-between bg-black bg-opacity-50 p-5 text-white font-bold opacity-0 transition-opacity duration-300 group-hover:opacity-100">
         <div className="transform -translate-y-10 group-hover:translate-y-0 transition-transform duration-500">
           <h3>{project.name}</h3>
           <p className="font-light text-xs">
